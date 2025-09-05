2e3f97d92f565875f376d1badea84afc46bbb418
liyuliang <endofalley@hotmail.com>
2025-09-05 14:43:29 +0800

    fix(android-tv): add okhttp logging-interceptor dependency to resolve HttpLoggingInterceptor import

--- android-tv/app/build.gradle.kts
+++ android-tv/app/build.gradle.kts
@@ -67,6 +67,7 @@ dependencies {
 
     // Networking placeholders (to be wired in M2)
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
     implementation("com.squareup.retrofit2:retrofit:2.11.0")
     implementation("com.squareup.retrofit2:converter-moshi:2.11.0")
 
